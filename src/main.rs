7f85041bd7fa870181b0ebce52b263cc99a1a020
Luna <14264545+mankeywitz@users.noreply.github.com>
2022-06-19 22:15:56 -0500

    add upload endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use rocket::fs::NamedFile;
 use rocket::futures::executor::block_on;
+use rocket::data::{Data, ToByteUnit};
+use rocket::tokio;
+use std::fs::File;
+use std::path::Path;
 
 #[macro_use] extern crate rocket;
 
@@ -20,7 +24,28 @@ fn download(titleid: String) -> Result<NamedFile, std::io::Error> {
     block_on(NamedFile::open(path))
 }
 
+#[post("/<id>/<titleid>/upload", data = "<data>")]
+async fn upload(id: String, titleid: String, data: Data<'_>) -> std::io::Result<()> {
+    println!("Console ID is {}", id);
+    println!("Title ID is {}", titleid);
+
+    let path = id + "/" + &titleid;
+
+    if !Path::new(&path).exists() {
+        std::fs::create_dir_all(Path::new(&path)).unwrap();
+    }
+
+    let f = File::create(path + "/msg.bin").unwrap();
+    let tokio_f = tokio::fs::File::from_std(f);
+
+    data.open(512.kibibytes())
+        .stream_to(tokio_f)
+        .await?;
+
+    Ok(())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, version, download])
+    rocket::build().mount("/", routes![index, version, download, upload])
 }
