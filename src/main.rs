2435b3c47c5f128fd064fec0ce17af64700459ac
Luna <14264545+mankeywitz@users.noreply.github.com>
2022-06-19 20:26:17 -0500

    add temp route for downloading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rocket::fs::NamedFile;
+use rocket::futures::executor::block_on;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -10,7 +13,14 @@ fn version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
 
+#[get("/<titleid>/download")]
+fn download(titleid: String) -> Result<NamedFile, std::io::Error> {
+    println!("Title ID is {}", titleid);
+    let path = "./sp-data/".to_owned() + &titleid + "/NT9DANjYeugA=";
+    block_on(NamedFile::open(path))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, version])
+    rocket::build().mount("/", routes![index, version, download])
 }
